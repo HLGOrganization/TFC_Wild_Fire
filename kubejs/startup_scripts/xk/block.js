8c93c7c27e034d6092a61572dce0ff2055ea7aef
xiankuang <a1369505035a@gmail.com>
2025-05-06 19:43:02 +0800

    修改鞍山齿轮箱的id

--- kubejs/startup_scripts/xk/block.js
+++ kubejs/startup_scripts/xk/block.js
@@ -1,5 +1,5 @@
 StartupEvents.registry("block",event=>{
-   event.create("create:andesite_Chassis")
+   event.create("create:andesite_chassis")
            .noValidSpawns(false) // 上面是否生成怪物
            .hardness(0.3) // 硬度
            .lightLevel(1)
