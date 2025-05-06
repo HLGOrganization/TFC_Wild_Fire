01cb4793c77b48f53ca64bee7060ec4d6d811d46
JasonQ <nmuqindexiang@163.com>
2025-05-06 19:26:00 +0800

    修改整合包名称为野火（wildfire），暂时注释掉无用配方

---
+++ kubejs/server_scripts/create/machine_making.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(e => {//手动合成
+    const { create } = e.recipes
+    // create.item_application("tfc_wf:andesite_chassis",['create:andesite_casing','#tfc:chisels'],600,"none",true)//手动合成安山机箱
+    // create.item_application('create:encased_fan',["tfc_wf:andesite_chassis",'wildfire:fan_blade'],600,"none",false)//手动合成鼓风机
+    // create.item_application('create:mechanical_press',["tfc_wf:andesite_chassis",'wildfire:wrought_iron_pressing_head'],600,"none",false)//手动合成辊压机
+    // create.item_application('create:mechanical_saw',["tfc_wf:andesite_chassis",'immersiveengineering:sawblade'],600,"none",false)//手动合成切割机
+    // create.item_application('create:mechanical_drill',["tfc_wf:andesite_chassis",'createoreexcavation:drill'],600,"none",false)//手动合成动力钻头
+    // create.item_application('create:mechanical_mixer',["tfc_wf:andesite_chassis",'create:whisk'],600,"none",false)//手动合成搅拌机
+    
+})
